681be920bda0ec128c403a4e5a0ae97637af7e3c
AyaJas <aya.hameedat@gmail.com>
2022-03-19 16:06:46 +0300

    Lab11 Solved

---
+++ src/main/java/com/example/songr/domain/Album.java
@@ -0,0 +1,75 @@
+package com.example.songr.domain;
+
+public class Album {
+//    title, an artist, a songCount, a length (in seconds), and an imageUrl that is a link to that album’s art.
+
+    private String title;
+    private String artist;
+    private int songCount;
+    private int length;
+    private String imageUrl;
+
+    public Album() {
+    }
+
+    public Album(String title, String artist, int songCount, int length, String imageUrl) {
+        this.title = title;
+        this.artist = artist;
+        this.songCount = songCount;
+        this.length = length;
+        this.imageUrl = imageUrl;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public void setSongCount(int songCount) {
+        this.songCount = songCount;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public int getSongCount() {
+        return songCount;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "Album{" +
+                "title='" + title + '\'' +
+                ", artist='" + artist + '\'' +
+                ", songCount=" + songCount +
+                ", length=" + length +
+                ", imageUrl='" + imageUrl + '\'' +
+                '}';
+    }
+}
+
+
--- src/main/java/com/example/songr/web/helloController.java
+++ src/main/java/com/example/songr/web/helloController.java
@@ -1,12 +1,13 @@
 package com.example.songr.web;
 
+import com.example.songr.domain.Album;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class helloController {
@@ -41,6 +42,19 @@ public class helloController {
         model.addAttribute("name", name.toUpperCase());
 
         return "capitalize";
+    }
+
+    @ResponseBody
+    @GetMapping("/Albums")
+    List<Album> getAllAlbums()
+    {
+        List<Album> albums = new ArrayList<>();
+        albums.add(new Album ("Insan","Hamza Namira",2,12,"./Hamza/Insan.png"));
+        albums.add(new Album ("Hateer min tani","Hamza Namira",3,7,"./Hamza/HateerMinTani.png"));
+        albums.add(new Album ("Esmaani","Hamza Namira",1,9,"./Hamza/ُEsmaani.png"));
+
+        return albums;
+
     }
 
 }
